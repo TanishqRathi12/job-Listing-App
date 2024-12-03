da9f56b11861b973756cb7a4c155bca0fa86f748
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-12-03 14:40:29 +0530

    initial commit

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,17 @@
+
+function Navigation() {
+  return (
+    <nav className="flex justify-between bg-white">
+        <div className="flex items-center">
+            <img src="/briefcase 1.png" alt="briefcase png" className="mr-2" />
+            <h3>RemoteJobs</h3>
+        </div>
+        <div className="flex items-center">
+            <img src="/BellRinging.png" alt="Bell png" className="mr-2" />
+            <img src="/Ellipse 18.png" alt="png" />
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation;
